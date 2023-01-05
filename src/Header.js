4921493a68cb3aee2f16ace08cbd11cf78d9f197
alina-henderson <alina.henders@gmail.com>
2023-01-05 12:40:34 -0800

    feat: upload logo

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import { ReactComponent as Logo } from "./Alisa Kornilova.svg";
+
+function Header() {
+	return (
+		<div>
+			<h1 className="header-name">Alisa Kornilova</h1>
+			<Logo />
+		</div>
+	);
+}
+
+export default Header;
