188efbf1d3b12af708fa14a14646d1abe0513e9a
alina-henderson <alina.henders@gmail.com>
2023-01-12 14:56:39 -0800

    Updates

--- src/Portrait.js
+++
@@ -1,7 +0,0 @@
-import portrait from "./images/portrait.jpg";
-
-function Portrait() {
-	return <img className="portrait" src={portrait} alt="Portrait" style={{ height:"100%", width:"100%", objectFit:"cover" }} />;
-}
-
-export default Portrait;
